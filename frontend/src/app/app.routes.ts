32f6547788176091dc2541cfe5a54201c14efbcf
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2023-12-07 19:20:42 +0000

    Add blog post component to app routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BlogPostComponent } from './modules/blog-post/blog-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'blog', component: BlogPostComponent},
+];
