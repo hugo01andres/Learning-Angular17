795b34e4b402464f74156f2ac01285b779aa971a
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2023-12-07 22:16:42 +0000

    Add contact form route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { BlogPostComponent } from './modules/blog-post/blog-post.component';
 import { PruebaComponent } from './modules/prueba/prueba.component';
+import { ContactComponent } from './modules/contact/contact.component';
 
 export const routes: Routes = [
     {path: 'blog-post', component: BlogPostComponent},
-    {path: 'prueba', component: PruebaComponent}
+    {path: 'prueba', component: PruebaComponent},
+    {path: 'contact', title: 'Contact form',component: ContactComponent}
 ];
