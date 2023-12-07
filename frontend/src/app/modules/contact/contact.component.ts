948e56592bc53427eb76a3aea149bdd846face68
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2023-12-07 22:24:50 +0000

    Add contact form to contact component

--- frontend/src/app/modules/contact/contact.component.ts
+++ frontend/src/app/modules/contact/contact.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  favouriteFramework: string = '';
 }
