1e5a64492a049df03223800e419895758b09d7c7
Hugo Martinez <72633652+hugo01andres@users.noreply.github.com>
2023-12-07 22:32:28 +0000

    Add reactive forms and sendForm() function

--- frontend/src/app/modules/contact/contact.component.ts
+++ frontend/src/app/modules/contact/contact.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
   favouriteFramework: string = '';
+
+  sendForm() {
+    console.log('Form sent!', this.favouriteFramework);
+  }
 }
